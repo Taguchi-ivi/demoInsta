dc18225a6c38e3b4722ce914089371a3aec94b68
Taguchi-ivi <k.taguchi@ivisystem.com>
2022-05-01 14:10:49 +0900

    csrfトークンの修正失敗のため、元に戻す

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     include ActionController::RequestForgeryProtection
     protect_from_forgery with: :exception
+    # skip_forgery_protection
 
 
     before_action :configure_permitted_parameters, if: :devise_controller?
