baf406564e93620078d75b171b37e113ea3d6387
Taguchi-ivi <k.taguchi@ivisystem.com>
2022-05-04 02:16:23 +0900

    article実装完了 jsでボタン振り分け操作まで成功

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,14 +2,22 @@ class ArticlesController < ApplicationController
     before_action :authenticate_user!
 
     def index
-
+        @articles = Article.all
     end
 
     def new
+        @article = current_user.articles.build
     end
     
 
     def create
+        @article = current_user.articles.build(article_params)
+        if @article.save
+            redirect_to root_path, notice: '保存できたよ'
+        else
+            flash.now[:error] = '保存に失敗したよ'
+            render :new
+        end
     end
 
     
@@ -20,5 +28,4 @@ class ArticlesController < ApplicationController
                 images: []
             )
         end
-
 end
--- app/javascript/packs/articleIndex.js
+++ app/javascript/packs/articleIndex.js
@@ -31,7 +31,7 @@ const myswiper = new Swiper(".swiper", {
 // })
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('hello');
+    // console.log('hello');
     myswiper();
 })
 
